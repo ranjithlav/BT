189b5451e66710a1dad2850d33d3d0654adf0076
Ranjith Kumar <ranjithlav@gmail.com>
2019-06-04 19:12:50 +0530

    Added DB scripts

---
+++ dbo.Addresses.sql
@@ -0,0 +1,26 @@
+﻿USE [BTPoc]
+GO
+
+/****** Object: Table [dbo].[Addresses] Script Date: 6/4/2019 5:52:40 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE [dbo].[Addresses];
+
+
+GO
+CREATE TABLE [dbo].[Addresses] (
+    [AddressId]   INT           IDENTITY (1, 1) NOT NULL,
+    [ContactId]   INT           NOT NULL,
+    [Street]      VARCHAR (500) NULL,
+    [City]        VARCHAR (150) NULL,
+    [State]       VARCHAR (100) NULL,
+    [Zipcode]     VARCHAR (10)  NULL,
+    [CreatedDate] DATETIME2 (7) NULL,
+    [UpdatedDate] DATETIME2 (7) NULL
+);
+
+
---
+++ dbo.Contacts.sql
@@ -0,0 +1,25 @@
+﻿USE [BTPoc]
+GO
+
+/****** Object: Table [dbo].[Contacts] Script Date: 6/4/2019 5:53:05 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE [dbo].[Contacts];
+
+
+GO
+CREATE TABLE [dbo].[Contacts] (
+    [ContactId]    INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]    VARCHAR (150) NULL,
+    [LastName]     VARCHAR (150) NULL,
+    [BusinessName] VARCHAR (500) NULL,
+    [Type]         VARCHAR (10)  NULL,
+    [CreatedDate]  DATETIME2 (7) NULL,
+    [UpdatedDate]  DATETIME2 (7) NULL
+);
+
+
